88987db4ef59acd9b60b0e68649cad27541a1453
Lachovski <145782520+LachovskiJohnnie@users.noreply.github.com>
2025-04-01 08:24:17 -0300

    01/04/25

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded,' function(){
-    const aumentaFonteBotao = document.getElementById(aumentar-fonte)
-
+document.addEventListener('DOMContentLoaded,' function () {
+    const aumentaFonteBotao = document.getElementById(aumentar - fonte)
 
     let tamanhoAtualFonte = 1;
-})
+
+
+    aumentarFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontsize = `${tamanhoAtualFonte}rem`;
+    });
+});
